66170c0a2ba452064e770b6cf69f1ee0261cc733
orenzhang <orenzhang@ovinc.cn>
2024-05-13 19:30:52 +0800

    fix: init route failed

--- ovinc_client/account/urls.py
+++ ovinc_client/account/urls.py
@@ -4,6 +4,6 @@ from ovinc_client.account.views import UserInfoViewSet, UserSignViewSet
 
 router = DefaultRouter()
 router.register("", UserSignViewSet)
-router.register("user_info", UserInfoViewSet)
+router.register("user_info", UserInfoViewSet, basename="user_info")
 
 urlpatterns = router.urls
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="ovinc_client",
-    version="0.2.3",
+    version="0.2.4",
     author="OVINC",
     url="https://www.ovinc.cn/",
     author_email="contact@ovinc.cn",
