fb6cfb5b26fcf84a0c39b1ccd7fff44032818013
Oren Zhang <orenzhang@ovinc.cn>
2024-06-15 19:44:42 +0800

    fix: loop is already running

--- ovinc_client/core/async_tools.py
+++ ovinc_client/core/async_tools.py
@@ -1,13 +1,27 @@
 import asyncio
+from threading import Thread
+from typing import Coroutine
 
 
-def sync_run(fn):
+class SyncRunner:
     """
     Run an async func in sync func
     """
 
-    loop = asyncio.get_event_loop()
-    if loop.is_running():
-        return loop.run_until_complete(fn)
-    else:
-        return asyncio.run(fn)
+    def __init__(self):
+        self.loop = asyncio.new_event_loop()
+        self.thread = Thread(target=self._run_loop_in_thread)
+        self.thread.start()
+
+    def _run_loop_in_thread(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
+
+    def run(self, co: Coroutine):
+        try:
+            future = asyncio.run_coroutine_threadsafe(co, self.loop)
+            return future.result()
+        finally:
+            self.loop.call_soon_threadsafe(self.loop.stop)
+            self.thread.join()
+            self.loop.close()
