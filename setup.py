9d5dfdb3d74f6ac45798d689ffeaeea2af5c88fb
Oren Zhang <orenzhang@ovinc.cn>
2024-06-15 18:48:01 +0800

    feat: run async func in sync func

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="ovinc_client",
-    version="0.3.2",
+    version="0.3.3b0",
     author="OVINC",
     url="https://www.ovinc.cn/",
     author_email="contact@ovinc.cn",
