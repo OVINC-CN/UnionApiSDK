5131779005a904a266245eb56f07adba902b2817
orenzhang <orenzhang@ovinc.cn>
2024-07-31 11:32:54 +0800

    chore: change version to rc

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="ovinc_client",
-    version="0.3.8b0",
+    version="0.3.8",
     author="OVINC",
     url="https://www.ovinc.cn/",
     author_email="contact@ovinc.cn",
