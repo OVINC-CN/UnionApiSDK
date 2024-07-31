b1eceb68a3e97cd4bff9e2e83e5111f545c9b434
orenzhang <orenzhang@ovinc.cn>
2024-07-31 11:26:56 +0800

    feat: ot instrument add more trace info

--- ovinc_client/trace/instrumentors.py
+++ ovinc_client/trace/instrumentors.py
@@ -1,43 +1,59 @@
-import json
 import traceback
 from typing import Collection, Union
 
 import MySQLdb
 from django.conf import settings
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from opentelemetry.instrumentation.celery import CeleryInstrumentor
 from opentelemetry.instrumentation.dbapi import (
     DatabaseApiIntegration,
     trace_integration,
 )
 from opentelemetry.instrumentation.django import DjangoInstrumentor
-from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
+from opentelemetry.instrumentation.httpx import (
+    HTTPXClientInstrumentor,
+    RequestInfo,
+    ResponseInfo,
+)
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.instrumentation.redis import RedisInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.trace import Span, Status, StatusCode, format_trace_id
-from requests import Response
+from opentelemetry.trace import Span, StatusCode, format_trace_id
+from redis import Redis
+from requests import PreparedRequest, Response
+from rest_framework import status
 
 from ovinc_client.core.logger import logger
 from ovinc_client.trace.constants import SPAN_DB_TYPE, SPAN_REDIS_TYPE, SpanAttributes
 
 
-def requests_callback(span: Span, response: Union[Response, HttpResponse]):
+def requests_hook(span: Span, request: PreparedRequest):
     """
-    Http Request Callback
+    Http Request Hook
     """
 
-    # Set Status Code
-    status_code = response.status_code
-    span.set_attribute("statusCode", status_code)
+    span.update_name(f"{request.method} {request.url}")
+    span.set_attributes(
+        attributes={
+            SpanAttributes.HTTP_URL: request.url,
+            SpanAttributes.HTTP_METHOD: request.method,
+        }
+    )
 
-    # Set Status
-    if status_code < 300:
-        span.set_status(Status(StatusCode.OK))
-    else:
-        span.set_status(Status(StatusCode.ERROR))
+
+def response_hook(span: Span, request: Union[PreparedRequest, HttpRequest], response: Union[Response, HttpResponse]):
+    """
+    HTTP Response Hook
+    """
+
+    span.set_attributes(
+        attributes={
+            SpanAttributes.HTTP_STATUS_CODE: response.status_code,
+        }
+    )
+    span.set_status(StatusCode.ERROR if response.status_code >= status.HTTP_400_BAD_REQUEST else StatusCode.OK)
 
 
 def django_request_hook(span: Span, request: WSGIRequest):
@@ -50,25 +66,12 @@ def django_request_hook(span: Span, request: WSGIRequest):
     request.otel_trace_id = format_trace_id(trace_id)
 
 
-def django_response_hook(span: Span, request: WSGIRequest, response: HttpResponse):
+def django_response_hook(span: Span, request: HttpRequest, response: HttpResponse):
     """
     Django Response Hook
     """
 
-    # Loads Response Content as Json
-    try:
-        result = json.loads(response.content)
-        if not isinstance(result, dict):
-            return
-    except Exception:  # pylint: disable=W0718
-        return
-
-    # Set Attributes
-    span.set_attribute(SpanAttributes.RESULT_MESSAGE, result.get("message", ""))
-    span.set_attribute(SpanAttributes.RESULT_ERRORS, result.get("errors", ""))
-
-    # Set Status
-    requests_callback(span, response)
+    response_hook(span, request, response)
 
 
 class DBApiIntegration(DatabaseApiIntegration):
@@ -96,21 +99,68 @@ class DBApiIntegration(DatabaseApiIntegration):
             logger.error(traceback.format_exc())
 
 
-class RedisRequestHook:
+def redis_request_hook(span: Span, instance: Redis, args, kwargs):
     """
     Redis Request Hook
     """
 
-    def __call__(self, span, instance, args, kwargs):
+    try:
+        connection_kwargs: dict = instance.connection_pool.connection_kwargs
+        host = connection_kwargs.get("host")
+        port = connection_kwargs.get("port")
+        db = connection_kwargs.get("db")
         span.set_attributes(
             {
-                SpanAttributes.DB_INSTANCE: f"{SPAN_REDIS_TYPE}({settings.REDIS_DB})",
-                SpanAttributes.DB_NAME: f"{SPAN_REDIS_TYPE}({settings.REDIS_DB})",
+                SpanAttributes.DB_INSTANCE: f"{host}/{db}",
+                SpanAttributes.DB_NAME: f"{host}/{db}",
                 SpanAttributes.DB_TYPE: SPAN_REDIS_TYPE,
-                SpanAttributes.DB_PORT: settings.REDIS_PORT,
-                SpanAttributes.DB_IP: settings.REDIS_HOST,
+                SpanAttributes.DB_PORT: port,
+                SpanAttributes.DB_IP: host,
+                SpanAttributes.DB_STATEMENT: " ".join([str(i) for i in args]),
+                SpanAttributes.DB_OPERATION: str(args[0]),
             }
         )
+    except Exception:  # pylint: disable=W0718
+        logger.error(traceback.format_exc())
+
+
+def httpx_request_hook(span: Span, request: RequestInfo):
+    """
+    HTTPX Request Hook
+    """
+
+    span.update_name(f"{request.method.decode()} {request.url}")
+    span.set_attributes(
+        attributes={
+            SpanAttributes.HTTP_URL: request.url,
+            SpanAttributes.HTTP_METHOD: request.method,
+        }
+    )
+
+
+def httpx_response_hook(span: Span, request: RequestInfo, response: ResponseInfo):
+    """
+    HTTPX Response Hook
+    """
+
+    span.set_attribute(SpanAttributes.HTTP_STATUS_CODE, response.status_code)
+    span.set_status(StatusCode.ERROR if response.status_code >= status.HTTP_400_BAD_REQUEST else StatusCode.OK)
+
+
+async def httpx_async_request_hook(span, request):
+    """
+    Async Request Hook
+    """
+
+    httpx_request_hook(span, request)
+
+
+async def httpx_async_response_hook(span, request, response):
+    """
+    Async Response Hook
+    """
+
+    httpx_response_hook(span, request, response)
 
 
 class Instrumentor(BaseInstrumentor):
@@ -123,11 +173,16 @@ class Instrumentor(BaseInstrumentor):
 
     def _instrument(self, **kwargs):
         LoggingInstrumentor().instrument()
-        RequestsInstrumentor().instrument(span_callback=requests_callback)
+        RequestsInstrumentor().instrument(request_hook=requests_hook, response_hook=response_hook)
         DjangoInstrumentor().instrument(request_hook=django_request_hook, response_hook=django_response_hook)
         CeleryInstrumentor().instrument()
-        RedisInstrumentor().instrument(request_hook=RedisRequestHook())
-        HTTPXClientInstrumentor().instrument()
+        RedisInstrumentor().instrument(request_hook=redis_request_hook)
+        HTTPXClientInstrumentor().instrument(
+            request_hook=httpx_request_hook,
+            response_hook=httpx_response_hook,
+            async_request_hook=httpx_async_request_hook,
+            async_response_hook=httpx_async_response_hook,
+        )
         trace_integration(MySQLdb, "connect", "mysql", db_api_integration_factory=DBApiIntegration)
 
     def _uninstrument(self, **kwargs):
