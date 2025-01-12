2dbd4d42c8a7f51d251960655e0be5e45f36ddc4
orenzhang <orenzhang@ovinc.cn>
2025-01-12 21:37:21 +0800

    chore(pypi): release 4.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="ovinc_client",
-    version="0.4.0b7",
+    version="0.4.0",
     author="OVINC",
     url="https://www.ovinc.cn/",
     author_email="contact@ovinc.cn",
